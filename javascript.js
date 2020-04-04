9d5a8d08987460c61b2ebdbb94788bb93c89ebc7
kayharriott <kayviaharriott@gmail.com>
2020-04-03 23:23:44 -0500

    Update index, added signup page and new CSS for signup page

--- javascript.js
+++ javascript.js
@@ -1,4 +1,9 @@
 
     function myFunction() {
-        alert("Hello World!");
+        //alert("Hello World!");
     }
+
+    function colorize() {
+        var element = document.getElementsByTagName("body")[0];
+        element.style.backgroundColor = 'blue';
+   }
