35bd4a3f6c616f03067e812b461e29af73b9b701
kayharriott <kayviaharriott@gmail.com>
2020-04-05 22:59:31 -0500

    Added faq, logos, and updated css

--- javascript.js
+++ javascript.js
@@ -7,3 +7,14 @@
         var element = document.getElementsByTagName("body")[0];
         element.style.backgroundColor = 'blue';
    }
+
+   function openForm(){
+       
+    document.getElementById("overlay").style.display = "block";
+       document.getElementById("signupForm").style.display = "block";
+    }
+
+   function closeForm(){
+       document.getElementById("signupForm").style.display = "none";
+       document.getElementById("overlay").style.display = "none";      
+    }
